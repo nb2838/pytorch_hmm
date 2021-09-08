c30ef5f307e54c3d6664146287344a9f2562298c
Nicolas Beltran <nb2838@columbia.edu>
2021-09-08 10:22:48 -0400

    working stable version for hmm

--- pytorch_hmm/models.py
+++ pytorch_hmm/models.py
@@ -2,12 +2,13 @@ import torch
 from torch.distributions.categorical import Categorical
 from torch.distributions.dirichlet import Dirichlet
 
+
 class HMM:
     def __init__(self, emission, hidden_dim, n, emission_dim=1, A=None,  pi=None):
         """
-        Arguments: 
+        Arguments:
         - hidden_dim: dimension of n
-        - n: length of the timeseries 
+        - n: length of the timeseries
         - A: matrix of transition probabilities
         - emission: An object representing emision distributions
         """
@@ -17,22 +18,22 @@ class HMM:
         self.hidden_dim = hidden_dim
         self.emission_dim = emission_dim
 
-        # Parameters of the model 
+        # Parameters of the model
         self.A = self.init_A(A)
         self.pi = self.init_pi(pi)
 
-        # Additional stuff 
-        self._log_likelihood = None 
+        # Additional stuff
+        self._log_likelihood = None
         self.emission = emission
-        
+
         assert self.A.shape == (hidden_dim, hidden_dim)
         assert self.pi.shape == (1, hidden_dim)
 
-    
-    def sample(self): 
+
+    def sample(self):
         """
-        Generates data from the hmm  and returns a tuple with 
-        latent and observed variables 
+        Generates data from the hmm  and returns a tuple with
+        latent and observed variables
         """
         z = [Categorical(self.pi).sample().item()]
         for i in range(1, self.n):
@@ -41,7 +42,7 @@ class HMM:
         x = []
         for i in range(len(z)):
             x.append(self.emission.sample(z[i]))
-            
+
         return torch.tensor(z), torch.tensor([x]).T
 
     def init_A(self, A):
@@ -52,7 +53,7 @@ class HMM:
 
     def init_pi(self, pi):
         if pi == None:
-            dirichlet = Dirichlet(torch.ones(1, self.hidden_dim) * 5)    
+            dirichlet = Dirichlet(torch.ones(1, self.hidden_dim) * 5)
             pi = dirichlet.sample()
         return pi
 
@@ -69,11 +70,11 @@ class HMM:
 
     def calculate_local_likelihoods(self,obs):
         """
-        Calculates the likelihoods p(x_n| z_n) and stores them in 
-        self.likelihoods. 
+        Calculates the likelihoods p(x_n| z_n) and stores them in
+        self.likelihoods.
 
-        self.likelihoods is a list of vectors of shape (hidden_dim,) 
-        where self.likelihoods[n][i] = p(x_n| z_n = i) 
+        self.likelihoods is a list of vectors of shape (hidden_dim,)
+        where self.likelihoods[n][i] = p(x_n| z_n = i)
         """
         likelihoods = []
         for i in range(len(obs)):
@@ -83,26 +84,24 @@ class HMM:
             likelihoods.append(torch.tensor(likelihood))
             assert likelihoods[-1].shape == (self.hidden_dim,)
         return likelihoods
-            
+
     def E_step(self, obs):
         """
         Implementation of the E step of the EM algorithm to calculate posterior
         p(z_n|X), p(z_n, z_{n-1}|X)
 
-        Implementation follows algorithm described in Pattern Recognition and 
+        Implementation follows algorithm described in Pattern Recognition and
         Machine Learning by Bishop. Assumes that we have called calculate_local_likelihoods
         """
         assert obs.shape == (self.n, self.emission_dim)
         likelihoods = self.calculate_local_likelihoods(obs)
-        
-        # First we will calculate the alphas
-        # TODO: Find a nicerway of dealing with loglikelihoods 
+
         alphas = []
         c = []
-        
-        self._log_likelihood  = 0 
+
+        self._log_likelihood  = 0
         for i in range(self.n):
-            if i == 0: 
+            if i == 0:
                 alpha = likelihoods[0] * self.pi[0]
                 c.append(torch.sum(alpha))
                 alphas.append(alpha/c[-1])
@@ -111,40 +110,38 @@ class HMM:
                 c.append(torch.sum(alpha))
                 alphas.append(alpha/c[-1])
 
-            self._log_likelihood += torch.log(c[-1]) 
-            assert alphas[-1].shape == (self.hidden_dim,) 
-        assert len(c) == self.n
+            self._log_likelihood += torch.log(c[-1])
+            assert alphas[-1].shape == (self.hidden_dim,)
+
         # Now we calculate the betas
         betas = [torch.ones(self.hidden_dim)]
         for i in range(self.n - 1, 0, -1):
-            beta =  torch.einsum('j,ij->i',likelihoods[i] *betas[-1], self.A)
+            beta =  torch.einsum('j,j,ij->i',likelihoods[i], betas[-1], self.A)
             betas.append(beta/c[i])
-            assert beta.shape  == (self.hidden_dim,) 
-
+            assert beta.shape  == (self.hidden_dim,)
         betas.reverse()
-        # Finally we return the suffcient stattistics 
+        
+        # Compute sufficient statistics 
         gammas = [torch.unsqueeze(alphas[i] * betas[i],0) for i in range(self.n)]
         chis = []
         for i in range(self.n-1):
             result = likelihoods[i+1] * betas[i+1]
             result = torch.outer(alphas[i], result)
             chis.append(torch.unsqueeze(result * self.A / c[i+1],0))
-
         return torch.cat(gammas, 0), torch.cat(chis, 0)
 
-    
     def M_step(self, obs, gammas, chis):
         """
         Does the M_Step of the maximization algorithm
         """
         assert gammas.shape == (self.n, self.hidden_dim)
-        assert chis.shape == (self.n-1, self.hidden_dim, self.hidden_dim) 
+        assert chis.shape == (self.n-1, self.hidden_dim, self.hidden_dim)
 
         self.pi = gammas[:1]/torch.sum(gammas[0])
         self.A = torch.sum(chis, axis=0)
         self.A = self.A/torch.unsqueeze(torch.sum(self.A, axis=-1), -1)
         self.emission.M_step(obs, gammas, chis)
-        
+
     def viterbi(self, obs):
         likelihoods = self.calculate_local_likelihoods(obs)
         omegas = [torch.log(self.pi[0]) + torch.log(likelihoods[0])]
@@ -155,7 +152,7 @@ class HMM:
             vals, index = torch.max(x, 0)
             omegas.append(vals + torch.log(likelihoods[i]))
             paths.append(index)
-            
+
         max_log_prob = torch.max(omegas[-1])
         likeley_states = [torch.argmax(omegas[-1])]
 
@@ -166,19 +163,23 @@ class HMM:
         likeley_states.reverse()
         likeley_states = [x.numpy() for x in likeley_states]
         return max_log_prob, likeley_states
- 
-        
+
+
     def fit(self, obs, eps):
         """
-        Performs MLE estimation on the paramaters of the HMM
+        Performs MLE estimation on the paramaters of the HMM using EM.
+        Arguments:
+        - obs: Tensor of shape [self.n, self.emision_dim] representing 
+          observed values
+        - eps: Required minimum improvement between iterations to keep
+          going
         """
-
         self.emission.init_params(obs)
-        
+
         old_log_likelihood = -float('INF')
         is_improvement = True
         step = 0
-        
+
         while is_improvement:
             gammas, chi = self.E_step(obs)
             self.M_step(obs, gammas, chi)
@@ -187,71 +188,3 @@ class HMM:
             old_log_likelihood = self.log_likelihood
             step += 1
             print(f'Step {step} - log_likelihood {self.log_likelihood.item()}')
-
-    
-            
-        
-        
-        
-                
-    
-        
-from emissions import NormalEmission
-
-hidden_dim = 2
-emission_dim = 1
-
-n = 3000
-
-mu = torch.unsqueeze(torch.arange(hidden_dim) * 10.0,-1)
-sigma = torch.ones(hidden_dim,1,1)* 1.0
-A = torch.tensor([[0.9, 0.1],[0.1,0.9]])
-
-emission = NormalEmission(hidden_dim=hidden_dim, emission_dim=emission_dim, mu=mu, sigma=sigma)
-hmm = HMM(n= n, emission=emission, hidden_dim=hidden_dim, A= A)
-pi = hmm.pi 
-z, x = hmm.sample()
-# hmm.E_step(x)
-
-# print('log_prob', hmm.log_likelihood)
-
-
-from hmmlearn import hmm as hmms
-model = hmms.GaussianHMM(n_components=2, covariance_type="full", n_iter=100, verbose=2,algorithm='viterbi')
-model.fit(x)
-log_prob, za_pred = model.decode(x, algorithm='viterbi')
-print('log_prob', log_prob)
-
-# model.startprob_ = pi[0].numpy()
-# model.means_ = hmm.mu.numpy()
-# model.covars_ = hmm.sigma.numpy()
-# model.transmat_ = hmm.A.numpy()
-# print(model._score(x.numpy(), compute_posteriors=True))
-
-
-
-
-hmm = HMM(n= n,hidden_dim=hidden_dim, emission=emission)
-print('A', hmm.A)
-print('sigma', sigma) 
-
-hmm.fit(x, 0.0001)
-log_prob, z_pred = hmm.viterbi(x)
-print('log_prob', log_prob)
-
-
-print(A)
-print(hmm.A)
-
-print(hmm.params)
-
-
-###############################
-
-import matplotlib.pyplot as plt
-from plot import plot_state_diagram
-
-print(x)
-plot_state_diagram(z_pred,x.numpy())
-plot_state_diagram(za_pred,x.numpy())
-
